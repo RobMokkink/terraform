a55a1e455314567e4a98d07e83c3c7294baea4fa
Rob Mokkink <rob@mokkinksystems.com>
2021-03-08 11:53:06 +0100

    fixes typo

--- libvirt/libvirt.tf
+++ libvirt/libvirt.tf
@@ -8,14 +8,22 @@ resource "random_shuffle" "master" {
   result_count = length(var.k8s_masters)
 }
 
-resource "random_shuffle" "workers" {
+resource "random_shuffle" "worker" {
   input = ["locA", "locB"]
   result_count = length(var.k8s_workers)
 }
 
-resource "libvirt_volume" "ubuntu18" {
+resource "libvirt_volume" "master_ubuntu18" {
   name = "${var.k8s_masters[count.index].name}.qcow2"
   count = length(var.k8s_masters)
+  source = "file:///var/lib/libvirt/images/ubuntu18.04-template"
+  pool = "default"
+  format = "qcow2"
+}
+
+resource "libvirt_volume" "worker_ubuntu18" {
+  name = "${var.k8s_workers[count.index].name}.qcow2"
+  count = length(var.k8s_workers)
   source = "file:///var/lib/libvirt/images/ubuntu18.04-template"
   pool = "default"
   format = "qcow2"
@@ -33,7 +41,7 @@ resource "libvirt_domain" "k8s_master_vms" {
   vcpu = 2
   description = random_shuffle.master.result[count.index]
   disk {
-    volume_id = libvirt_volume.ubuntu18[count.index].id
+    volume_id = libvirt_volume.master_ubuntu18[count.index].id
   }
 }
 
@@ -47,8 +55,8 @@ resource "libvirt_domain" "k8s_worker_vms" {
   name = var.k8s_workers[count.index].name
   memory = "4096"
   vcpu = 2
-  description = random_shuffle.workers.result[count.index]
+  description = random_shuffle.worker.result[count.index]
   disk {
-    volume_id = libvirt_volume.ubuntu18[count.index].id
+    volume_id = libvirt_volume.worker_ubuntu18[count.index].id
   }
 }
